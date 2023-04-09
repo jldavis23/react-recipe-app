18fc9f3ad26ba0638befa611375463b61def40ea
Jacey Davis <jaceyd923@gmail.com>
2023-04-08 22:49:42 -0600

    user can view recipes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import './App.css';
 import recipeData from './data/recipes.json'
 import { useState } from 'react'
 
+let id = 1
+
 export default function App() {
   const [recipes, setRecipes] = useState(recipeData)
+  //console.log(recipes)
+  console.log(recipes)
+
+  let recipeComponents = []
+  recipes.forEach(recipe => {
+    recipeComponents.push(<Recipe recipe={recipe} key={id++}/>)
+  })
 
   return (
     <main>
+      <h1><span>Yum</span>Recipes</h1>
 
+      <div className="browse-section">
+        <h2>Browse</h2>
+        <div className="recipes-wrapper">
+          {recipeComponents}
+        </div>
+      </div>
+
+      <div className="add-recipes-section"></div>
     </main>
   )
 }
 
-function Hello() {
+function Recipe( {recipe} ) {
   return (
-    <p>Hello</p>
+    <figure className="recipe-card">
+      <div className="image-div">
+        <img src={recipe.imageURL} alt={recipe.name}></img>
+      </div>
+      <figcaption>
+        <h3>{recipe.name}</h3>
+
+      </figcaption>
+      
+    </figure>
   )
 }

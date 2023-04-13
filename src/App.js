54570ef8d7dc85bca173159831868e8f13b6d117
Jacey Davis <jaceyd923@gmail.com>
2023-04-13 16:41:32 -0600

    user can add recipes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ let id = 1
 
 export default function App() {
   const [recipes, setRecipes] = useState(recipeData)
-  //console.log(recipes)
   console.log(recipes)
+
 
   let recipeComponents = []
   recipes.forEach(recipe => {
-    recipeComponents.push(<Recipe recipe={recipe} key={id++}/>)
+    recipeComponents.push(<Recipe recipe={recipe} key={id++} />)
   })
 
   return (
@@ -25,12 +25,16 @@ export default function App() {
         </div>
       </div>
 
-      <div className="add-recipes-section"></div>
+      <div className="add-recipes-section">
+        <h2>Add a Recipe</h2>
+
+        <AddNewRecipe recipes={recipes} setRecipes={setRecipes}/>
+      </div>
     </main>
   )
 }
 
-function Recipe( {recipe} ) {
+function Recipe({ recipe }) {
   return (
     <figure className="recipe-card">
       <div className="image-div">
@@ -40,7 +44,108 @@ function Recipe( {recipe} ) {
         <h3>{recipe.name}</h3>
 
       </figcaption>
-      
+
     </figure>
+  )
+}
+
+function AddNewRecipe({recipes, setRecipes}) {
+  const [newRecipe, setNewRecipe] = useState({
+    name: '', ingredients: [''], steps: ['']
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //console.log(newRecipe)
+    setRecipes([...recipes, newRecipe])
+    setNewRecipe({
+      name: '', ingredients: [''], steps: ['']
+    })
+  }
+
+  let ingredientInputs = []
+  let IngIndex = 0
+  for (let i = 0; i < newRecipe.ingredients.length; i++) {
+    ingredientInputs.push(<Ingredient newRecipe={newRecipe} setNewRecipe={setNewRecipe} index={IngIndex} key={IngIndex}/>)
+    IngIndex++
+  }
+  const addIngredient = () => {
+    setNewRecipe({...newRecipe, ingredients: [...newRecipe.ingredients, '']})
+  }
+
+  let stepInputs = []
+  let stepIndex = 0
+  for (let i = 0; i < newRecipe.steps.length; i++) {
+    stepInputs.push(<Step newRecipe={newRecipe} setNewRecipe={setNewRecipe} index={stepIndex} key={stepIndex}/>)
+    stepIndex++
+  }
+  const addStep = () => {
+    setNewRecipe({...newRecipe, steps: [...newRecipe.steps, '']})
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Recipe Name: <br></br>
+        <input type="text" value={newRecipe.name} onChange={(e) => {setNewRecipe({ ...newRecipe, name: e.target.value})}}></input>
+      </label>
+
+      <label>Ingredients: </label>
+      {ingredientInputs}
+      <button type="button" onClick={addIngredient}>Add Ingredient</button>
+
+      <label>Steps: </label>
+      <button type="button" onClick={addStep}>Add Step</button>
+      {stepInputs}
+      
+
+      <button type="submit">Add Recipe</button>
+    </form>
+  )
+}
+
+function Ingredient({newRecipe, setNewRecipe, index}) {
+  const handleChange = (e) => {
+    let newArray = newRecipe.ingredients
+    newArray[index] = e.target.value
+    setNewRecipe({...newRecipe, ingredients: newArray})
+  }
+
+  const deleteIngredient = () => {
+    let newArray = newRecipe.ingredients.toSpliced(index, 1)
+    setNewRecipe({...newRecipe, ingredients: newArray})
+  }
+
+  return (
+    <>
+      <input type="text" value={newRecipe.ingredients[index]} onChange={handleChange}></input>
+      <button type="button" onClick={deleteIngredient}>remove</button>
+    </>
+    
+  )
+}
+
+function Step({newRecipe, setNewRecipe, index}) {
+  const handleChange = (e) => {
+    let newArray = newRecipe.steps
+    newArray[index] = e.target.value
+    setNewRecipe({...newRecipe, steps: newArray})
+  }
+
+  const deleteStep = () => {
+    let newArray = newRecipe.steps.toSpliced(index, 1)
+    setNewRecipe({...newRecipe, steps: newArray})
+  }
+
+  return (
+    <>
+      <div>
+        <span>{index+1}.</span>
+        <input type="text" value={newRecipe.steps[index]} onChange={handleChange}></input>
+      </div>
+      <button type="button" onClick={deleteStep}>remove</button>
+    </>
+    
   )
 }
